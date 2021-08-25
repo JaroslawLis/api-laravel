3fcc9a3124f52ee0ce357434fc8f8477fa1485f8
JaroslawLis <niejarek@poczta.onet.pl>
2021-08-25 07:27:41 +0200

    create tables

--- database/migrations/2021_08_25_051555_create_opinions_table.php
+++ database/migrations/2021_08_25_051555_create_opinions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOpinionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('opinions', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
-            $table->string('surname', 100);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer("book_id")->unsigned();
+            $table->integer("note");
+            $table->text("descritpion");
+            $table->string("author", 100);
+            $table->string('email');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('opinions');
     }
 }
